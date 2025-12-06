3448483fb116fb8b71999613a64e01b5b218d322
Salwa-pratama <nandev1915@gmail.com>
2025-12-06 15:55:15 +0700

    perbaikan kesalahan penulisan komponen

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,10 +12,10 @@
 
 <body class="min-h-full">
     {{-- Ini bagian Header --}}
-    <x-navbar></x-navbar>
+    <x-nav-bar></x-nav-bar>
     <x-header>{{ $title }}</x-header>
     <main>
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 ">
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
             {{ $slot }}
         </div>
     </main>
